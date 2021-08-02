2d105c5ae804fd14cee9c32483da671c5edbcbca
Chris Dekker <chris@krakend.nl>
2021-08-02 17:43:43 +0200

    -- Refactored in 2 hours

--- php/Game.php
+++ php/Game.php
@@ -1,178 +1,225 @@
 <?php
-function echoln($string) {
-  echo $string."\n";
+
+function echo_ln( $string ) {
+    echo "$string\n";
 }
 
-class Game {
-    var $players;
-    var $places;
-    var $purses ;
-    var $inPenaltyBox ;
+class Players
+{
+    private $players = [];
+    private $current_player = 0;
 
-    var $popQuestions;
-    var $scienceQuestions;
-    var $sportsQuestions;
-    var $rockQuestions;
+    public function add_player( $player_name ): int
+    {
+        array_push( $this->players, $player_name );
 
-    var $currentPlayer = 0;
-    var $isGettingOutOfPenaltyBox;
+        return $this->get_player_count();
+    }
 
-    function  __construct(){
+    public function get_player_count(): int
+    {
+        return count( $this->players );
+    }
 
-   	$this->players = array();
-        $this->places = array(0);
-        $this->purses  = array(0);
-        $this->inPenaltyBox  = array(0);
+    public function get_current_player()
+    {
+        return $this->players[ $this->current_player ];
+    }
 
-        $this->popQuestions = array();
-        $this->scienceQuestions = array();
-        $this->sportsQuestions = array();
-        $this->rockQuestions = array();
+    public function get_current_player_id(): int
+    {
+        return $this->current_player;
+    }
 
-        for ($i = 0; $i < 50; $i++) {
-			array_push($this->popQuestions, "Pop Question " . $i);
-			array_push($this->scienceQuestions, ("Science Question " . $i));
-			array_push($this->sportsQuestions, ("Sports Question " . $i));
-			array_push($this->rockQuestions, $this->createRockQuestion($i));
+    public function set_next_player()
+    {
+        if ( ( $this->current_player + 1 ) === count( $this->players ) ) {
+            $this->current_player = 0;
+        } else {
+            $this->current_player++;
+        }
+    }
+}
+
+class Game
+{
+    private $is_getting_out_of_penalty_box;
+
+    private $places = [0],
+            $purses = [0],
+            $in_penalty_box = [0];
+            
+    private $pop_questions = [],
+            $science_questions = [],
+            $sports_questions = [],
+            $rock_questions = [];
+
+    private $players;
+
+    public function  __construct() {
+        $this->players = new Players();
+
+        for ( $i = 0; $i < 50; $i++ ) {
+			array_push( $this->pop_questions, "Pop Question $i" );
+			array_push( $this->science_questions, "Science Question $i" );
+			array_push( $this->sports_questions, "Sports Question $i" );
+			array_push( $this->rock_questions, "Rock Question $i" );
     	}
     }
 
-	function createRockQuestion($index){
-		return "Rock Question " . $index;
+    private function player_has_penalty( $player ): int
+    {
+        return $this->in_penalty_box[ $player ];
+    }
+
+	private function set_player_position( $roll )
+    {
+        $current_position = $this->places[ $this->players->get_current_player_id() ];
+
+        if ( ( $current_position + $roll ) > 11 ) {
+            $this->places[ $this->players->get_current_player_id() ] = ( ( $current_position + $roll ) - 12 );
+        } else {
+            $this->places[ $this->players->get_current_player_id() ] += $roll;
+        }
+    }
+
+    private function get_player_position(): int
+    {
+        return $this->places[ $this->players->get_current_player_id() ];
+    }
+
+	private function ask_question() {
+		if ( $this->get_current_category() === "Pop" )
+			echo_ln( array_shift( $this->pop_questions ) );
+
+		if ( $this->get_current_category() === "Science" )
+			echo_ln( array_shift( $this->science_questions ) );
+		
+		if ( $this->get_current_category() === "Sports" )
+			echo_ln( array_shift( $this->sports_questions ) );
+
+		if ( $this->get_current_category() === "Rock" )
+			echo_ln( array_shift( $this->rock_questions ) );
 	}
 
-	function isPlayable() {
-		return ($this->howManyPlayers() >= 2);
-	}
 
-	function add($playerName) {
-	   array_push($this->players, $playerName);
-	   $this->places[$this->howManyPlayers()] = 0;
-	   $this->purses[$this->howManyPlayers()] = 0;
-	   $this->inPenaltyBox[$this->howManyPlayers()] = false;
+	private function get_current_category(): string
+    {
+        $current_position = $this->get_player_position();
 
-	    echoln($playerName . " was added");
-	    echoln("They are player number " . count($this->players));
-		return true;
-	}
+        switch( $current_position ) {
+            case 0:
+            case 4:
+            case 8:
+                return "Pop";
 
-	function howManyPlayers() {
-		return count($this->players);
-	}
+            case 1:
+            case 5:
+            case 9:
+                return "Science";
 
-	function  roll($roll) {
-		echoln($this->players[$this->currentPlayer] . " is the current player");
-		echoln("They have rolled a " . $roll);
+            case 2:
+            case 6:
+            case 10:
+                return "Sports";
 
-		if ($this->inPenaltyBox[$this->currentPlayer]) {
-			if ($roll % 2 != 0) {
-				$this->isGettingOutOfPenaltyBox = true;
+            default:
+                return "Rock";
+        }
+    }
 
-				echoln($this->players[$this->currentPlayer] . " is getting out of the penalty box");
-			$this->places[$this->currentPlayer] = $this->places[$this->currentPlayer] + $roll;
-				if ($this->places[$this->currentPlayer] > 11) $this->places[$this->currentPlayer] = $this->places[$this->currentPlayer] - 12;
+    private function send_player_to_penalty_box()
+    {
+        $this->in_penalty_box[ $this->players->get_current_player_id() ] = true;
+    }
 
-				echoln($this->players[$this->currentPlayer]
-						. "'s new location is "
-						.$this->places[$this->currentPlayer]);
-				echoln("The category is " . $this->currentCategory());
-				$this->askQuestion();
-			} else {
-				echoln($this->players[$this->currentPlayer] . " is not getting out of the penalty box");
-				$this->isGettingOutOfPenaltyBox = false;
-				}
+    private function did_player_win(): bool
+    {
+        return ! ( $this->get_player_purse() == 6 );
+    }
 
+    private function add_to_player_purse()
+    {
+        $this->purses[ $this->players->get_current_player_id() ]++;
+    }
+
+    private function get_player_purse(): int
+    {
+        return $this->purses[ $this->players->get_current_player_id() ];
+    }
+
+    public function add_player( $player_name )
+    {
+        $player_id = $this->players->add_player( $player_name );
+
+        $this->places[ $player_id ] = 0;
+        $this->purses[ $player_id ] = 0;
+        $this->in_penalty_box[ $player_id ] = false;
+    }
+
+    public function roll( $roll ) {
+        $current_player = $this->players->get_current_player();
+
+        echo_ln( "$current_player is the current player" );
+        echo_ln( "They have rolled a $roll" );
+
+        $has_penalty = $this->player_has_penalty( $this->players->get_current_player_id() );
+        if ( $has_penalty ) {
+            if ( ( $roll % 2 ) !== 0 ) {
+                $this->is_getting_out_of_penalty_box = true;
+
+                echo_ln( "$current_player is getting out of the penalty box" );
+            } else {
+                echo_ln( "$current_player is not getting out of the penalty box" );
+
+                $this->is_getting_out_of_penalty_box = false;
+
+                return;
+            }
+        }
+
+        $this->set_player_position( $roll );
+        $new_position = $this->get_player_position();
+
+        echo_ln( "$current_player's new location is $new_position" );
+        echo_ln( "The category is " . $this->get_current_category() );
+
+        $this->ask_question();
+    }
+
+	public function was_correctly_answered(): bool
+    {
+        $player_has_penalty = $this->player_has_penalty( $this->players->get_current_player_id() );
+        $current_player = $this->players->get_current_player();
+        
+		if ( $player_has_penalty && ! $this->is_getting_out_of_penalty_box ) {
+            $this->players->set_next_player();
+            return true;
 		} else {
+			echo_ln( "Answer was correct!!!!" );
 
-		$this->places[$this->currentPlayer] = $this->places[$this->currentPlayer] + $roll;
-			if ($this->places[$this->currentPlayer] > 11) $this->places[$this->currentPlayer] = $this->places[$this->currentPlayer] - 12;
+			$this->add_to_player_purse();
+            $player_gold_count = $this->get_player_purse();
 
-			echoln($this->players[$this->currentPlayer]
-					. "'s new location is "
-					.$this->places[$this->currentPlayer]);
-			echoln("The category is " . $this->currentCategory());
-			$this->askQuestion();
-		}
+            echo_ln( "$current_player now has $player_gold_count Gold Coins." );
 
-	}
-
-	function  askQuestion() {
-		if ($this->currentCategory() == "Pop")
-			echoln(array_shift($this->popQuestions));
-		if ($this->currentCategory() == "Science")
-			echoln(array_shift($this->scienceQuestions));
-		if ($this->currentCategory() == "Sports")
-			echoln(array_shift($this->sportsQuestions));
-		if ($this->currentCategory() == "Rock")
-			echoln(array_shift($this->rockQuestions));
-	}
-
-
-	function currentCategory() {
-		if ($this->places[$this->currentPlayer] == 0) return "Pop";
-		if ($this->places[$this->currentPlayer] == 4) return "Pop";
-		if ($this->places[$this->currentPlayer] == 8) return "Pop";
-		if ($this->places[$this->currentPlayer] == 1) return "Science";
-		if ($this->places[$this->currentPlayer] == 5) return "Science";
-		if ($this->places[$this->currentPlayer] == 9) return "Science";
-		if ($this->places[$this->currentPlayer] == 2) return "Sports";
-		if ($this->places[$this->currentPlayer] == 6) return "Sports";
-		if ($this->places[$this->currentPlayer] == 10) return "Sports";
-		return "Rock";
-	}
-
-	function wasCorrectlyAnswered() {
-		if ($this->inPenaltyBox[$this->currentPlayer]){
-			if ($this->isGettingOutOfPenaltyBox) {
-				echoln("Answer was correct!!!!");
-			$this->purses[$this->currentPlayer]++;
-				echoln($this->players[$this->currentPlayer]
-						. " now has "
-						.$this->purses[$this->currentPlayer]
-						. " Gold Coins.");
-
-				$winner = $this->didPlayerWin();
-				$this->currentPlayer++;
-				if ($this->currentPlayer == count($this->players)) $this->currentPlayer = 0;
-
-				return $winner;
-			} else {
-				$this->currentPlayer++;
-				if ($this->currentPlayer == count($this->players)) $this->currentPlayer = 0;
-				return true;
-			}
-
-
-
-		} else {
-
-			echoln("Answer was corrent!!!!");
-		$this->purses[$this->currentPlayer]++;
-			echoln($this->players[$this->currentPlayer]
-					. " now has "
-					.$this->purses[$this->currentPlayer]
-					. " Gold Coins.");
-
-			$winner = $this->didPlayerWin();
-			$this->currentPlayer++;
-			if ($this->currentPlayer == count($this->players)) $this->currentPlayer = 0;
+            $winner = $this->did_player_win();
+            $this->players->set_next_player();
 
 			return $winner;
 		}
 	}
 
-	function wrongAnswer(){
-		echoln("Question was incorrectly answered");
-		echoln($this->players[$this->currentPlayer] . " was sent to the penalty box");
-	$this->inPenaltyBox[$this->currentPlayer] = true;
+	public function was_answered_wrong(): bool
+    {
+        $current_player = $this->players->get_current_player();
 
-		$this->currentPlayer++;
-		if ($this->currentPlayer == count($this->players)) $this->currentPlayer = 0;
+		echo_ln( "Question was incorrectly answered" );
+		echo_ln( "$current_player was sent to the penalty box" );
+
+		$this->send_player_to_penalty_box();
+		$this->players->set_next_player();
+
 		return true;
-	}
-
-
-	function didPlayerWin() {
-		return !($this->purses[$this->currentPlayer] == 6);
 	}
 }
--- php/GameRunner.php
+++ php/GameRunner.php
@@ -1,27 +1,24 @@
 <?php
 
-include __DIR__.'/Game.php';
+    include_once __DIR__ . '/Game.php';
 
-$notAWinner;
+    $notAWinner = true;
 
-  $aGame = new Game();
-  
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
-  
-  
-  do {
-    
-    $aGame->roll(rand(0,5) + 1);
-    
-    if (rand(0,9) == 7) {
-      $notAWinner = $aGame->wrongAnswer();
-    } else {
-      $notAWinner = $aGame->wasCorrectlyAnswered();
-    }
-    
-    
-    
-  } while ($notAWinner);
+    $game = new Game();
+
+    $game->add_player( "Chet" );
+    $game->add_player( "Pat" );
+    $game->add_player( "Sue" );
+
+    do {
+
+        $game->roll( rand( 0, 5 ) + 1 );
+
+        if ( rand( 0, 9 ) === 7 ) {
+            $notAWinner = $game->was_answered_wrong();
+        } else {
+            $notAWinner = $game->was_correctly_answered();
+        }
+
+    } while ($notAWinner);
   
